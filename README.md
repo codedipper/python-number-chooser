8a56a3860c3e8dfa604c6301c371dada696bb2e9
codedipper <59625582+codedipper@users.noreply.github.com>
2020-08-07 18:16:03 -0400

    Added files.
    Added files for this little Python project.

---
+++ README.md
@@ -0,0 +1,12 @@
+# python-number-chooser
+## About
+My first Python project!\
+This project uses Python 3.8.5.\
+Older version of Python should work.
+## Instructions
+Configure the `min` variable to the desired minimum number.\
+Configure the `max` variable to the desired maximum number.\
+Simple as that.
+## Notes
+- Code explanation is in the `main.py` file.\
+- You can run the `run.bat` file for easy execution on Windows.
---
+++ main.py
@@ -0,0 +1,11 @@
+import random # Here, I imported "random".
+
+min = 100 # Create variable "min" with the specified number.
+max = 150 # Create variable "max" with the specified number.
+
+num = random.randrange(min, max) # Get "random", since it is imported.
+# Then create a random range between "min" and "max".
+# And make that all into the "num" variable.
+
+print("The number is..") # Print "The number is..".
+print(num) # Print the variable "num".
