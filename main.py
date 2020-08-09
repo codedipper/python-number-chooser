3c1103a20e44b11b12450c9c964b509430cb3d60
codedipper <59625582+codedipper@users.noreply.github.com>
2020-08-09 14:59:21 -0400

    Put the notes on the next line.
    I put the notes on the next line to clean up the code and so it doesn't flood the command prompt when an error happens.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import random # Here, I imported "random".
+import random
+# Here, I imported "random".
 
 minchoice = input("Enter the minimum number and press enter.")
 # Print some text then wait for input.
-maxchoice = float(input("Enter maximum number and press enter."))
+maxchoice = input("Enter maximum number and press enter.")
 # Print some text then wait for input.
 
-num = random.randrange(int(minchoice), int(maxchoice)) # Set the number as the inputs.
+num = random.randrange(int(minchoice), int(maxchoice))
+# Set the number as the inputs.
 
-print("The number is..") # Print "The number is..".
-print(num) # Print the variable "num".
+print("The number is..")
+# Print "The number is..".
+
+print(num)
+# Print the variable "num".
