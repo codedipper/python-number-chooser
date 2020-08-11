a358ed6e9f1c9b9f9efe03b3b45ec2434433b064
codedipper <59625582+codedipper@users.noreply.github.com>
2020-08-11 16:20:57 -0400

    made things cleaner, fixed bugs.

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import random
-# Here, I imported "random".
+# Import "random".
 
 minchoice = input("Enter the minimum number and press enter.")
-# Print some text then wait for input.
+# Print some text then wait for input, then set as variable.
 maxchoice = input("Enter maximum number and press enter.")
-# Print some text then wait for input.
+# Print some text then wait for input, then set as variable.
 
-num = random.randrange(int(minchoice), int(maxchoice))
-# Set the number as the inputs.
+minnum = int(minchoice)
+# Create variable "minnum" as the integer of "minchoice".
+maxnum = int(maxchoice) + 1
+# Add 1 to the pre-stated "maxchoice" variable (to fix bugs).
+
+num = random.randrange(minnum, maxnum)
+# Set the "num" variable to require random,
+# to set the randrange from "minnum" to "maxnum".
 
 print("The number is..")
-# Print "The number is..".
+# Print some text.
 
 print(num)
 # Print the variable "num".
