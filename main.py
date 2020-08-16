fe9c294747232274a0f4055016c155490b0dfeb8
codedipper <59625582+codedipper@users.noreply.github.com>
2020-08-16 13:47:53 -0400

    Added aesthetics.

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import random
 # Import "random".
 
-print("| Enter the minimum number and press enter.")
-minnum = int(input())
+print("[System]: | Enter the minimum number and press enter.\n")
+minnum = int(input("[You]: | "))
 # Print some text then wait for input, then set as variable and convert to integer.
 
-print("| Enter maximum number and press enter.")
-maxnum = int(input())
+print("\n[System]: | Enter maximum number and press enter.\n")
+maxnum = int(input("[You]: | "))
 # Print some text then wait for input, then set as variable and convert to integer.
 
 num = str(random.randrange(minnum, maxnum))
 # Set the "num" variable to require random,
 # to set the randrange from "minnum" to "maxnum" and convert to string.
 
-print("| The number is "+ num +".")
+print("\n[System]: | The number is "+ num +".\n")
 # Print some text and attach the "num" variable.
